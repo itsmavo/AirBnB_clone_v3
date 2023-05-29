c0f5d1facc13752f88e7344ec4264aeb9154cd39
itsmavo <mansolo.osolo51@gmail.com>
2023-05-29 16:35:01 +0000

    revert city

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -12,7 +12,7 @@ from models.city import City
 @app_views.route('/states/<state_id>/cities', methods=['GET', 'DELETE', 'PUT'],
 		strict_slashes=False)
 def city(city_id):
-    city = storage.get(City, city_id)
+    city = storage.get('City', city_id)
     if not city:
         abort (404)
 
@@ -36,11 +36,11 @@ def city(city_id):
 
 @app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'], strict_slashes=False)
 def cities_of_State(state_id):
-    state = storage.get(State, state_id)
+    state = storage.get('State', state_id)
     if not state:
         abort(404)
     if request.method == 'GET':
-        state = storage.get(State, state_id)
+        state = storage.get('State', state_id)
         return jsonify([city.to_dict() for city in state.cities])
 
     if request.method == 'POST':
